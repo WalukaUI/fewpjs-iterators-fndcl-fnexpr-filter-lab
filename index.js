03c53f9fe96b70b23055de07c408a11cba771a05
WalukaUI <cwaluka@yahoo.com>
2021-04-02 21:30:36 -0700

    all tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+function findMatching(dri,str){
+    return dri.filter(function(match){
+        return match.toLowerCase() === str.toLowerCase()
+    })
+}
+
+function fuzzyMatch(dri,str){
+    return dri.filter(function(match){
+
+        return match.toLowerCase().indexOf(str.toLowerCase()) === 0
+    })
+}
+
+
+function matchName(dri,str){
+    return dri.filter(function(match){
+
+        return (match.name === str)
+    })
+}
